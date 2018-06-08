c6727d16be93de3d7bcda173a588b8dad882b6df
Luke Stebbing <ls@lunis.net>
2018-06-07 21:11:50 -0400

    Actually wait for tests to finish

--- test.mjs
+++ test.mjs
@@ -3,13 +3,12 @@ import {setConnector} from './model.mjs'
 const tests = []
 
 export function test(f) {
-    tests.push(async () => f())
+    tests.push(f)
 }
 
 export async function run(getConnector, ...modulePaths) {
     setConnector(getConnector({temporary: true}))
     await Promise.all(['./model.test.mjs', ...modulePaths].map(m => import(m)))
-    tests.map(f => f())
-    await Promise.all(tests)
+    await Promise.all(tests.map(f => f()))
     console.log(`${tests.length} pass`)
 }
