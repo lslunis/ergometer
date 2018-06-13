1c6db7897b8794d06d945ebe2530825ec6944d0c
Luke Stebbing <ls@lunis.net>
2018-06-13 10:35:52 -0400

    Add debugger config

--- test.js
+++ test.js
@@ -10,5 +10,4 @@ export async function run(getConnector, ...modulePaths) {
     setConnector(getConnector({temporary: true}))
     await Promise.all(['./model.test.js', ...modulePaths].map(m => import(m)))
     await Promise.all(tests.map(f => f()))
-    document.body.textContent = `${tests.length} pass`
 }
