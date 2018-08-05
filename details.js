ad96c26bd2397535817aec3aa366e92582d11108
Luke Stebbing <ls@lunis.net>
2018-08-05 17:32:40 -0400

    Add basic style to details.html.

--- details.js
+++ details.js
@@ -1,3 +1,11 @@
+import {black, white} from './colors.js'
+
+{
+  const {style} = document.body
+  style.color = black
+  style.background = white
+}
+
 const port = browser.runtime.connect(
   null,
   {name: 'details'},
