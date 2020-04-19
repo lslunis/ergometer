95ea551b2283631cd59f62b48efd00c8994a968e
Luke Stebbing <ls@lunis.net>
2020-04-19 10:17:04 -0400

    Implement read_subprocess and ChangeEvent

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
-* https://devblogs.microsoft.com/oldnewthing/20160627-00/?p=93755
-* https://docs.microsoft.com/en-us/windows/win32/api/shellapi/ns-shellapi-notifyicondataa
-* https://docs.microsoft.com/en-us/windows/win32/menurc/using-icons
+- https://devblogs.microsoft.com/oldnewthing/20160627-00/?p=93755
+- https://docs.microsoft.com/en-us/windows/win32/api/shellapi/ns-shellapi-notifyicondataa
+- https://docs.microsoft.com/en-us/windows/win32/menurc/using-icons
+- https://github.com/Tinche/aiofiles/
--- ergometer.py
+++ ergometer.py
@@ -1,8 +1,30 @@
-from asyncio import sleep
-from time import time
+import asyncio
 
-async def read_input():
+
+async def read_subprocess(cmd, broker):
+    proc = await asyncio.create_subprocess_exec(
+        cmd,
+        stdin=asyncio.subprocess.DEVNULL,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.DEVNULL,
+    )
     while True:
-        now = round(time())
-        await emit(f'{now} 1 h\n')
-        await sleep(2)
+        message = await proc.stdout.readline()
+        if not message.endswith("\n"):
+            raise ValueError(f"truncated message: {message!r}")
+        broker.publish(message.strip())
+
+
+class Broker:
+    def publish(message):
+        pass
+
+
+class ChangeEvent(Event):
+    def __init__():
+        super().__init__()
+        self.set()
+
+    async def wait():
+        await super().wait()
+        self.clear()
