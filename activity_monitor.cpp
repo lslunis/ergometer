826a86c3cd5966ba1b0f77301a6122995e8900d0
Stephan T. Lavavej <stl@nuwen.net>
2020-04-19 18:20:15 -0700

    Add Raymond Chen's scratch code plus timer code

---
+++ activity_monitor.cpp
@@ -0,0 +1,202 @@
+// g++ -Wall -Wextra -std=c++17 -mwindows activity_monitor.cpp -lcomctl32 -lole32 -o activity_monitor.exe
+
+#define STRICT
+#include <windows.h>
+#include <windowsx.h>
+#include <ole2.h>
+#include <commctrl.h>
+#include <shlwapi.h>
+#include <strsafe.h>
+
+constexpr UINT_PTR IDT_TIMER1 = 1;
+
+HINSTANCE g_hinst;                          /* This application's HINSTANCE */
+HWND g_hwndChild;                           /* Optional child window */
+/*
+ *  OnSize
+ *      If we have an inner child, resize it to fit.
+ */
+void
+OnSize([[maybe_unused]] HWND hwnd, [[maybe_unused]] UINT state, int cx, int cy)
+{
+    if (g_hwndChild) {
+        MoveWindow(g_hwndChild, 0, 0, cx, cy, TRUE);
+    }
+}
+/*
+ *  OnCreate
+ *      Applications will typically override this and maybe even
+ *      create a child window.
+ */
+BOOL
+OnCreate(HWND hwnd, [[maybe_unused]] LPCREATESTRUCT lpcs)
+{
+  g_hwndChild = CreateWindow(TEXT("listbox"), NULL,
+      LBS_HASSTRINGS | WS_CHILD | WS_VISIBLE | WS_VSCROLL,
+      0, 0, 0, 0, hwnd, NULL, g_hinst, 0);
+
+  RAWINPUTDEVICE dev;
+  dev.usUsagePage = 1;
+  dev.usUsage = 6;
+  dev.dwFlags = 0;
+  dev.hwndTarget = hwnd;
+  RegisterRawInputDevices(&dev, 1, sizeof(dev));
+
+  SetTimer(hwnd, IDT_TIMER1, 1000, nullptr);
+
+  return TRUE;
+}
+/*
+ *  OnDestroy
+ *      Post a quit message because our application is over when the
+ *      user closes this window.
+ */
+void
+OnDestroy(HWND hwnd)
+{
+  RAWINPUTDEVICE dev;
+  dev.usUsagePage = 1;
+  dev.usUsage = 6;
+  dev.dwFlags = RIDEV_REMOVE;
+  dev.hwndTarget = hwnd;
+  RegisterRawInputDevices(&dev, 1, sizeof(dev));
+
+  KillTimer(hwnd, IDT_TIMER1);
+
+  PostQuitMessage(0);
+}
+
+#define HANDLE_WM_INPUT(hwnd, wParam, lParam, fn) \
+  ((fn)((hwnd), GET_RAWINPUT_CODE_WPARAM(wParam), \
+        (HRAWINPUT)(lParam)), 0)
+
+void OnInput([[maybe_unused]] HWND hwnd, [[maybe_unused]] WPARAM code, HRAWINPUT hRawInput)
+{
+  UINT dwSize;
+  GetRawInputData(hRawInput, RID_INPUT, nullptr,
+                  &dwSize, sizeof(RAWINPUTHEADER));
+  RAWINPUT *input = (RAWINPUT *)malloc(dwSize);
+  GetRawInputData(hRawInput, RID_INPUT, input,
+                  &dwSize, sizeof(RAWINPUTHEADER));
+  if (input->header.dwType == RIM_TYPEKEYBOARD) {
+    TCHAR prefix[80];
+    prefix[0] = TEXT('\0');
+    if (input->data.keyboard.Flags & RI_KEY_E0) {
+        StringCchCat(prefix, ARRAYSIZE(prefix), TEXT("E0 "));
+    }
+    if (input->data.keyboard.Flags & RI_KEY_E1) {
+        StringCchCat(prefix, ARRAYSIZE(prefix), TEXT("E1 "));
+    }
+
+    TCHAR buffer[256];
+    StringCchPrintf(buffer, ARRAYSIZE(buffer),
+        TEXT("%p, msg=%04x, vk=%04x, scanCode=%s%02x, %s"),
+        input->header.hDevice,
+        input->data.keyboard.Message,
+        input->data.keyboard.VKey,
+        prefix,
+        input->data.keyboard.MakeCode,
+        (input->data.keyboard.Flags & RI_KEY_BREAK)
+            ? TEXT("release") : TEXT("press"));
+    ListBox_AddString(g_hwndChild, buffer);
+  }
+  DefRawInputProc(&input, 1, sizeof(RAWINPUTHEADER));
+  free(input);
+}
+
+/*
+ *  PaintContent
+ *      Interesting things will be painted here eventually.
+ */
+void
+PaintContent([[maybe_unused]] HWND hwnd, [[maybe_unused]] PAINTSTRUCT *pps)
+{
+}
+/*
+ *  OnPaint
+ *      Paint the content as part of the paint cycle.
+ */
+void
+OnPaint(HWND hwnd)
+{
+    PAINTSTRUCT ps;
+    BeginPaint(hwnd, &ps);
+    PaintContent(hwnd, &ps);
+    EndPaint(hwnd, &ps);
+}
+/*
+ *  OnPrintClient
+ *      Paint the content as requested by USER.
+ */
+void
+OnPrintClient(HWND hwnd, HDC hdc)
+{
+    PAINTSTRUCT ps;
+    ps.hdc = hdc;
+    GetClientRect(hwnd, &ps.rcPaint);
+    PaintContent(hwnd, &ps);
+}
+/*
+ *  Window procedure
+ */
+LRESULT CALLBACK
+WndProc(HWND hwnd, UINT uiMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uiMsg) {
+    HANDLE_MSG(hwnd, WM_CREATE, OnCreate);
+    HANDLE_MSG(hwnd, WM_SIZE, OnSize);
+    HANDLE_MSG(hwnd, WM_DESTROY, OnDestroy);
+    HANDLE_MSG(hwnd, WM_PAINT, OnPaint);
+    HANDLE_MSG(hwnd, WM_INPUT, OnInput);
+    case WM_PRINTCLIENT: OnPrintClient(hwnd, (HDC)wParam); return 0;
+
+    case WM_TIMER: ListBox_AddString(g_hwndChild, TEXT("TIMER")); return 0;
+
+    }
+    return DefWindowProc(hwnd, uiMsg, wParam, lParam);
+}
+BOOL
+InitApp(void)
+{
+    WNDCLASS wc;
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = g_hinst;
+    wc.hIcon = NULL;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = TEXT("Scratch");
+    if (!RegisterClass(&wc)) return FALSE;
+    InitCommonControls();               /* In case we use a common control */
+    return TRUE;
+}
+int WINAPI WinMain(HINSTANCE hinst, [[maybe_unused]] HINSTANCE hinstPrev,
+                   [[maybe_unused]] LPSTR lpCmdLine, int nShowCmd)
+{
+    MSG msg;
+    HWND hwnd;
+    g_hinst = hinst;
+    if (!InitApp()) return 0;
+    if (SUCCEEDED(CoInitialize(NULL))) {/* In case we use COM */
+        hwnd = CreateWindow(
+            TEXT("Scratch"),                /* Class Name */
+            TEXT("Scratch"),                /* Title */
+            WS_OVERLAPPEDWINDOW,            /* Style */
+            CW_USEDEFAULT, CW_USEDEFAULT,   /* Position */
+            CW_USEDEFAULT, CW_USEDEFAULT,   /* Size */
+            NULL,                           /* Parent */
+            NULL,                           /* No menu */
+            hinst,                          /* Instance */
+            0);                             /* No special parameters */
+        ShowWindow(hwnd, nShowCmd);
+        while (GetMessage(&msg, NULL, 0, 0)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        CoUninitialize();
+    }
+    return 0;
+}
