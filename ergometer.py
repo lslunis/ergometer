94995a6e3162117cf8e8eb771764976634b8d063
Luke Stebbing <ls@lunis.net>
2020-04-22 19:17:06 -0400

    Define schema

--- ergometer.py
+++ ergometer.py
@@ -1,3 +1,4 @@
+import aiosqlite
 import asyncio
 
 
@@ -13,6 +14,60 @@ async def read_subprocess(cmd, broker):
         if not message.endswith("\n"):
             raise ValueError(f"truncated message: {message!r}")
         broker.publish(message.strip())
+
+
+async def commit_state(broker):
+    async with aiosqlite.connect("state.db") as db:
+        await db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS "offsets" (
+                "host" INTEGER PRIMARY KEY,
+                "offset" INTEGER NOT NULL
+            )"""
+        )
+        await db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS "targets" (
+                "name" TEXT PRIMARY KEY,
+                "target" INTEGER NOT NULL,
+                "mtime" INTEGER NOT NULL
+            ) WITHOUT ROWID"""
+        )
+        await db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS "totals" (
+                "start" INTEGER PRIMARY KEY,
+                "total" INTEGER NOT NULL
+            )"""
+        )
+        await db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS "gaps" (
+                "start" INTEGER PRIMARY KEY,
+                "span" INTEGER NOT NULL
+            )"""
+        )
+        await db.execute('''DROP INDEX IF EXISTS "rests"''')
+
+        async with db.execute(
+            """SELECT "target" FROM "targets" WHERE "name" = 'r'"""
+        ) as cursor:
+            async for (rest_target,) in cursor:
+                pass
+        await db.execute(
+            f"""
+            CREATE UNIQUE INDEX "rests" ON "gaps" ("start")
+                WHERE "span" >= {rest_target}"""
+        )
+        offsets = []
+        async with db.execute(
+            '''SELECT "host", "offset" FROM "offsets"'''
+        ) as cursor:
+            async for host, offset in cursor:
+                offsets.append((host, offset))
+        await db.commit()
+        async for x in broker.subscribe(offsets):
+            pass
 
 
 class Broker:
