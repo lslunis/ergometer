657255dad2937715e9c89d1c51ef20bda1681de5
jeff <50226+dayorbyte@users.noreply.github.com>
2020-05-16 11:50:08 -0400

    Add a main file to data_processor.py
    This change also:
    
    * changes die() to check() because I got confused about which way the boolean goes
    * adds a fake subprocess to read from as a demo
    * fixes a variety of bugs I found when I tried to run this

--- data_processor.py
+++ data_processor.py
@@ -1,8 +1,12 @@
+import platform
 import asyncio
+import asyncio.subprocess
 import glob
-import os.path as path
 import os
+import os.path as path
 import sys
+import uuid
+
 
 # trim at startup
 # mark irrecoverable data
@@ -13,44 +17,47 @@ class IntegrityError(Exception):
 
 
 # Read local events for "host", writing them to "broker"
-async def publish_local_events(host, file_manager, broker):
+async def publish_local_events(host, broker, file_manager):
+    print("publish_local_events")
     position = await broker.host_position(host)
     while True:
-        data = await file_manager.read(position)
+        data = await file_manager.read(host, position, 100)
         # Handles reconnect internally.
         position = await broker.write(host, data, position)
 
 
-def die(check, msg):
-    if not check:
+def check(check, msg):
+    if check:
         return
     print(msg, file=sys.stderr)
-    os.exit(1)
+    sys.exit(1)
 
 
 # Read local events from "event_queue" in groups of "batch_size"
 # and write them using "file_manager"
 async def local_event_handler(host, event_queue, file_manager, batch_size):
     while True:
-        to_write = [event_queue.get()]
+        to_write = [await event_queue.get()]
         for _ in range(batch_size - 1):
             if event_queue.empty():
                 break
             # Make this a list
             to_write.append(event_queue.get_nowait())
-        file_manager.write(host, data)
+        file_manager.write(host, to_write)
 
 
 # Read all changes from "broker" for other hosts and write them using
 # "file_manager"
-async def change_subscriber(broker, file_manager):
+async def change_subscriber(self_host, broker, file_manager):
     while True:
         # Initialize.
-        positions = await file_manager.positions()
+        positions = file_manager.positions
         try:
             # Read messages forever.
-            async for host, data, position in broker.read(positions, exclude=host):
-                file_manager.write(host, data, position=position)
+            async for data_host, data, position in broker.read(
+                positions, exclude=self_host
+            ):
+                file_manager.write(data_host, data, position=position)
         except Exception as e:
             file_manager.log(e)
 
@@ -62,32 +69,36 @@ class HostFile:
         self.path = host_path
         self.data_available = asyncio.Event()
         self.error_event = error_event
+        if not path.exists(host_path):
+            with open(host_path, "w"):
+                pass
 
     @property
     def size(self):
-        return os.stat(host_path).st_size
+        return os.stat(self.path).st_size
 
     # REQUIRES: f is a file opened in "r+" mode
     def safe_seek(self, f):
         file_position = self.size
+        mod = file_position % 16
         if file_position % 16 != 0:
             file_position -= file_position % 16
             f.seek(file_position)
             corruption = f.read(16)
             self.log(f"Corruption in file {self.path}: '{corruption}'")
-            f.seek(file_position)
+        f.seek(file_position)
         return file_position
 
     # data is an iterable of bytes objects.
     def write(self, data, position=None):
         for d in data:
-            die(type(d) == bytes, "Got non-bytes data to write: {d}")
+            check(type(d) == bytes, f"Got non-bytes data to write: {d}")
         # Open for reading and writing without truncating.
         with open(self.path, "rb+") as f:
             file_position = self.safe_seek(f)
             to_write = b"".join(data)
             to_write_len = len(to_write)
-            if position != file_position:
+            if position != file_position and position is not None:
                 raise IntegrityError(
                     f"Tried to write at position {position} but the latest safe position is {file_position}"
                 )
@@ -96,7 +107,9 @@ class HostFile:
                     f"Tried to write {to_write_len} bytes to {self.path}, which is not divisible by 16"
                 )
             bytes_written = f.write(to_write)
-            die(bytes_written == len(data), f"Failed to write to file {self.path}")
+            check(
+                bytes_written == len(to_write), f"Failed to write to file {self.path}"
+            )
             self.data_available.set()
             self.data_available = asyncio.Event()
 
@@ -106,19 +119,19 @@ class HostFile:
         with open(error_path, "a") as f:
             full_message = msg + "\n"
             byte_written = f.write(full_message)
-            die(
+            check(
                 bytes_written == len(full_message),
                 f"Failed to write to error log {error_path}",
             )
         self.error_event.set()
 
     async def read(self, position, batch_size):
-        die(
+        check(
             position % 16 == 0,
             f"Tried to read file {self.path} at position {position} which is not divisible by 16",
         )
         file_size = self.size
-        die(
+        check(
             position <= file_size,
             f"Tried to read file {self.path} at position {position} which > {file_size}",
         )
@@ -146,12 +159,14 @@ class HostFile:
 
 # Reads and writes data for particular hosts.
 class FileManager:
-    def __init__(self, host, storage_root):
+    def __init__(self, host, storage_root, error_event):
         self.storage_root = storage_root
         self.host = host
+        self.error_event = error_event
+        self.hosts = {}
         for host_path in glob.glob(self.host_path("*")):
             host, _, _ = host_path.rpartition(".hostlog")
-            self.hosts[host] = HostFile(host_path)
+            self.hosts[host] = HostFile(host_path, self.error_event)
 
     def host_path(self, host):
         return os.path.join(self.storage_root, f"{host}.hostlog")
@@ -161,15 +176,93 @@ class FileManager:
         positions = {name: host.size for name, host in self.hosts.items()}
         # If any files have an integrity error ignore the corrupt part. It
         # will be logged later.
-        return {name: pos - (pos % 16) for name, pos in positions}
+        return {name: pos - (pos % 16) for name, pos in positions.items()}
 
     def write(self, host, data, position=None):
-        die(data[-1] == "\n", "TODO")
         if host not in self.hosts:
-            self.hosts[host] = HostFile(self.host_path(host))
+            self.hosts[host] = HostFile(self.host_path(host), self.error_event)
         self.hosts[host].write(data, position)
 
     async def read(self, host, position, batch_size):
         if host not in self.hosts:
-            self.hosts[host] = HostFile(self.host_path(host))
-        return self.hosts[host].read(position, batch_size)
+            self.hosts[host] = HostFile(self.host_path(host), self.error_event)
+        return await self.hosts[host].read(position, batch_size)
+
+
+class BrokerClient:
+    def __init__(self, address):
+        self.address = address
+        self.queue = asyncio.Queue(maxsize=1000)
+
+    async def read(self, positions, exclude=None):
+        # yields (host, data, position) tuples
+        await asyncio.sleep(99999)
+        yield (host, data, position)
+
+    async def write(self, host, data, position):
+        # returns the position of the broker for this host.
+        await asyncio.sleep(1)
+        return position + len(data)
+
+    async def host_position(self, host):
+        return 0
+
+
+def get_current_host(storage_root):
+    host_path = os.path.join(storage_root, "self.host")
+    if path.exists(host_path):
+        with open(host_path) as f:
+            return f.read()
+    host = str(uuid.uuid4())
+    with open(host_path, "w") as f:
+        f.write(host)
+    return host
+
+
+async def run_subprocess(queue, command, args):
+    subprocess = await asyncio.create_subprocess_exec(
+        command, *args, stdout=asyncio.subprocess.PIPE
+    )
+    while True:
+        data = await subprocess.stdout.read(16)
+        check(
+            len(data) == 16,
+            f"Did not get 16 bytes from subprocess {command}. Got: '{data}'",
+        )
+        await asyncio.sleep(0.5)
+        await queue.put(data)
+
+
+async def main():
+    storage_root = sys.argv[1]
+    cloud_broker_address = sys.argv[2]
+
+    error_event = asyncio.Event()
+    host = get_current_host(storage_root)
+    check(len(host) > 0, "host is empty")
+    file_manager = FileManager(host, storage_root, error_event)
+
+    # Set up cloud change manager.
+    broker = BrokerClient(cloud_broker_address)
+    subscriber = asyncio.create_task(change_subscriber(host, broker, file_manager))
+
+    # Set up the local change publisher.
+    publisher = asyncio.create_task(publish_local_events(host, broker, file_manager))
+
+    # Set up a subprocess listener
+    subprocess_queue = asyncio.Queue()
+    subprocess = asyncio.create_task(
+        run_subprocess(subprocess_queue, "cat", ["/dev/random"])
+    )
+    local_events = asyncio.create_task(
+        local_event_handler(host, subprocess_queue, file_manager, 100)
+    )
+
+    await asyncio.gather(subscriber, publisher, subprocess, local_events)
+
+
+if __name__ == "__main__":
+    if platform.system() == "Windows":
+        loop = asyncio.ProactorEventLoop()
+        asyncio.set_event_loop(loop)
+    asyncio.get_event_loop().run_until_complete(main())
