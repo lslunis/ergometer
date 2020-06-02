8086a5467d7e6f6e1fb433822dda286502845282
Luke Stebbing <ls@lunis.net>
2020-06-02 19:56:46 -0400

    Only build cpp on changes

--- dev.py
+++ dev.py
@@ -1,3 +1,10 @@
+import os
 from subprocess import run
 
-run("build_activity_monitor.bat")
+
+def mtime(path):
+    return os.stat(path).st_mtime_ns
+
+
+if mtime("activity_monitor.exe") < mtime("activity_monitor.cpp"):
+    run("build_activity_monitor.bat")
