faea686cf38d4dd39c1d17ff91e065b34839cfbf
konstell <jasminermj@gmail.com>
2020-06-07 11:39:15 -0400

    dev.py working on Ubuntu 19.10

--- dev.py
+++ dev.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 import venv
 from subprocess import run
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+is_windows = os.name == "nt"
+bin_dir = os.path.join("venv", "Scripts" if is_windows else "bin")
 
 
 def mtime(path):
@@ -11,33 +15,34 @@ def mtime(path):
 
 
 def build(*args):
-    if mtime("activity_monitor.exe") < mtime("activity_monitor.cpp"):
+    if is_windows and mtime("activity_monitor.exe") < mtime("activity_monitor.cpp"):
         run("build_activity_monitor.bat")
 
     if not os.path.exists("venv/pyvenv.cfg"):
         venv.create("venv", with_pip=True)
 
-    run("venv/Scripts/pip install -q -r requirements.txt")
+    run(f"{bin_dir}/pip install -q -r requirements.txt".split())
 
 
 def freeze(*args):
-    run(r"venv\Scripts\pip freeze > requirements.txt", shell=True)
+    pip = os.path.join(bin_dir, "pip")
+    run(f"{pip} freeze > requirements.txt", shell=True)
 
 
 def test(*args):
-    run(["venv/Scripts/pytest", *args])
+    run([f"{bin_dir}/pytest", *args])
 
 
 def upgrade(*args):
     lines = (
-        run("venv/Scripts/pip list --format freeze --outdated", capture_output=True)
+        run(f"{bin_dir}/pip list --format freeze --outdated".split(), capture_output=True)
         .stdout.decode("ascii")
         .splitlines()
     )
     packages = " ".join(line.partition("==")[0] for line in lines)
     if not packages:
         return
-    run(f"venv/Scripts/python -m pip install --upgrade {packages}")
+    run(f"{bin_dir}/python -m pip install --upgrade {packages}".split())
     freeze()
 
 
