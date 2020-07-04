fab774594cb58a6c1ad6557bcdcec6e8376d4748
konstell <jasminermj@gmail.com>
2020-07-04 17:56:43 -0400

    we have windows now!

--- client_main.py
+++ client_main.py
@@ -6,6 +6,7 @@ import wx
 import wx.adv
 
 from .model import Model
+from .time import precise_clock
 from .util import init, log
 
 Color = wx.Colour
@@ -53,9 +54,23 @@ class Tray(wx.adv.TaskBarIcon):
         top.SetTransparent(randint(0, 254))
 
 
+def show_settings(settings, push_local_event):
+    frame = wx.Frame(top)
+    sizer = wx.FlexGridSizer(3, 16, 16)
+    frame.SetSizer(sizer)
+    for name, value in settings.items():
+        name_text = wx.StaticText(frame, label=name.capitalize().replace("_", " "))
+        value_text = wx.StaticText(frame, label=str(value))
+        sizer.Add(name_text)
+        sizer.Add(value_text)
+        text_ctrl = wx.TextCtrl(frame)
+        sizer.Add(text_ctrl)
+        log.info(value_text.GetSize())
+    frame.Show()
+
 def draw(*args):
-    # log.debug("draw")
-    ...
+    metrics = model.metrics_at(precise_clock().timestamp())
+    log.debug(metrics)
 
 
 def exit(*args):
@@ -69,6 +84,7 @@ def exit(*args):
 
 if __name__ == "__main__":
     init()
+    log.setLevel(20)
     app = wx.App()
     style = wx.TRANSPARENT_WINDOW | wx.STAY_ON_TOP | wx.FRAME_TOOL_WINDOW | wx.MAXIMIZE
     top = wx.Frame(None, style=style) if os.name == "nt" else wx.Frame(None)
@@ -76,9 +92,12 @@ if __name__ == "__main__":
     top.Show()
     tray = Tray()
     model = Model()
+    settings = {"daily_target": 1800, "session_target": 300}
+    show_settings(settings, model.push_local_event)
     timer = wx.Timer(top)
     top.Bind(wx.EVT_TIMER, draw, timer)
     top.Bind(wx.EVT_CLOSE, exit)
     hz = wx.Display().GetCurrentMode().refresh or 60
+    hz = 2
     timer.Start(int(1000 / hz))
     app.MainLoop()
