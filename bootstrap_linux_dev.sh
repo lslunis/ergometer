fd2009632ae56143bb6e6d43dc3ccacd1c84f554
Patrick <patrick.a.lawson@gmail.com>
2020-07-05 03:25:20 +0000

    Add linux/docker bootstrapping script

---
+++ bootstrap_linux_dev.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo apt install build-essential tmux mosh python37 python37-dev
